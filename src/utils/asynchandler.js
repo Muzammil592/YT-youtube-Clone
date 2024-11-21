681414318219e563666cd224657c16e78de42206
Muzammil Abbas <132131721+Muzammil592@users.noreply.github.com>
2024-11-21 20:00:22 +0500

    Api response and Api Error

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,18 @@
+// const asynchandler=(fn)=>async(req,res,next)=>{
+//  try {
+//     await fn(req,res,next)
+//  } catch (error) {
+//     res.status(500).json({
+//         success:false,
+//         message:err.message
+//     })
+//  }
+// }
+
+const asynchandler=(requesthandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requesthandler(req,res,next)).catch((err)=> next(err))
+    }
+}
+
+export default asynchandler
